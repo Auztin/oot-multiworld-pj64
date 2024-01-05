8ddda8f7096ab9e3c7de33f81389d0d2f60cf094
Austin <>
2024-01-05 13:43:48 -0600

    rando 8.0 changes

--- Multiworld.js
+++ Multiworld.js
@@ -1,10 +1,21 @@
-var VERSION = '1.0.3rc2';
-var player_names_addr = 0;
+var VERSION = '1.0.4';
 var save_context = 0x8011A5D0;
-var oot = {ready:false,currentMode:-1};
-var consoleMode = 0;
+var oot = {
+  ready:false,
+  currentMode:-1,
+  player_names_addr:0,
+  mw_progressive_items_state:0,
+  version:'',
+  time:'',
+  worlds:0,
+  hash:[],
+  name:'',
+  inventory:0
+};
+var consoleMode = 1;
 var room = {};
 var connection = null;
+var connectionReady = false;
 var changed = false;
 var repairList = [];
 var defeatedGanon = false;
@@ -14,20 +25,57 @@ function inArray(val, array) {
   return array.indexOf(val) >= 0
 }
 
+var to_oot = [
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xE4,0xEA,0xDF,
+  0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xAB,0xAC,0xAD,0xAE,0xAF,0xB0,0xB1,0xB2,0xB3,0xB4,0xB5,0xB6,0xB7,0xB8,0xB9,
+  0xBA,0xBB,0xBC,0xBD,0xBE,0xBF,0xC0,0xC1,0xC2,0xC3,0xC4,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xC5,0xC6,0xC7,0xC8,0xC9,0xCA,0xCB,0xCC,0xCD,0xCE,0xCF,0xD0,0xD1,0xD2,0xD3,
+  0xD4,0xD5,0xD6,0xD7,0xD8,0xD9,0xDA,0xDB,0xDC,0xDD,0xDE,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,
+  0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF,0xDF
+];
+var from_oot = [
+  '0','1','2','3','4','5','6','7','8','9',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','A','B','C','D','E',
+  'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U',
+  'V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k',
+  'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ',
+  ' ',' ',' ',' ','-',' ',' ',' ',' ',' ','.',' ',' ',' ',' ',' ',
+  ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '
+];
+
 function ootEncode(str, len) {
   var ret = [];
-  for (var i=0; i<str.length && i<len; i++) {
-    var c = str[i].charCodeAt(0);
-    if (c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0)) c -= '0'.charCodeAt(0);
-    else if (c >= 'A'.charCodeAt(0) && c <= 'Z'.charCodeAt(0)) c += 0x6A;
-    else if (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) c += 0x64;
-    else if (c == '.'.charCodeAt(0)) c = 0xEA;
-    else if (c == '-'.charCodeAt(0)) c = 0xE4;
-    else if (c == ' '.charCodeAt(0)) c = 0xDF;
-    else continue;
-    ret.push(c);
-  }
+  for (var i=0; i<str.length && i<len; i++) ret.push(to_oot[str[i].charCodeAt(0)]);
   for (; i<len; i++) ret.push(0xDF);
+  return ret;
+}
+
+function ootDecode(i, trim) {
+  var ret = "";
+  for (var shift = 0; shift <= 24; shift += 8) {
+    var c = from_oot[(i >> shift) & 0xFF];
+    if (trim && c == ' ') continue;
+    ret = c+ret;
+  }
   return ret;
 }
 
@@ -50,24 +98,25 @@ function loadState() {
     room = JSON.parse(fs.readfile(saveFile));
   } catch (e) {
     room = {
-      server: 'mw.auztin.net',
-      port: 9001,
+      server: '',
+      port: 0,
       name: '',
       ids: {},
       sent: {},
-      received: []
+      received: [],
+      inventory: {}
     };
   }
   console.clear();
-  if (!room.server || !room.port || !room.name || !room.ids[oot.player_id]) {
-    room.name = '';
+  if (!room.server || !room.port || !room.ids[oot.player_id]) {
     setupRoomCfg();
   }
 }
 
 function setupRoom() {
   if (connection) return;
-  if (room.server && room.port && room.name && oot.ready && room.ids[oot.player_id]) {
+  if (room.server && room.port && oot.ready && room.ids[oot.player_id]) {
+    connectionReady = false;
     connection = new Socket();
     connection.connect(room.port, room.server, function() {
       connection.pingTimeout = false;
@@ -113,12 +162,18 @@ function setupRoom() {
                   CRC.push((crc & 0x00FF0000) >> 16)
                   CRC.push((crc & 0x0000FF00) >> 8)
                   CRC.push((crc & 0x000000FF))
-                  CRC = new Buffer(CRC);
-                  var cmd = new Buffer([
-                    0x03,
-                    room.name.length
-                  ]);
-                  connection.write(Buffer.concat([cmd, new Buffer(room.name), new Buffer([oot.player_id]), CRC]));
+                  connection.write(Buffer.concat([
+                    new Buffer([
+                      0x03,
+                      oot.version.length,
+                      oot.time.length,
+                      oot.worlds
+                    ].concat(oot.hash)),
+                    new Buffer(oot.version),
+                    new Buffer(oot.time),
+                    new Buffer([oot.player_id]),
+                    new Buffer(CRC)
+                  ]));
                 }
               }
             }
@@ -134,6 +189,14 @@ function setupRoom() {
                 if (room.ids[oot.player_id]) name = room.ids[oot.player_id];
                 connection.write(Buffer.concat([new Buffer([0x04, name.length]), new Buffer(name)]));
                 connection.write(new Buffer([0x06, (room.received.length & 0xFF00) >> 8, (room.received.length & 0x00FF)]));
+                connection.write(new Buffer([
+                  0x07,
+                  (oot.inventory & 0xFF000000) >> 24,
+                  (oot.inventory & 0x00FF0000) >> 16,
+                  (oot.inventory & 0x0000FF00) >> 8,
+                  (oot.inventory & 0x000000FF),
+                ]));
+                connectionReady = true;
               }
             }
             else if (state == 1) {
@@ -263,6 +326,32 @@ function setupRoom() {
               }
             }
           }
+          else if (command == 0x07) {
+            if (state == 0) {
+              bytes = data[i];
+              state = 1;
+              // extra = [data[i]];
+              // bytes = 0;
+              // bytesNeeded = 4;
+              // state = 1;
+            }
+            else if (state == 1) {
+              extra = [(bytes << 8) | data[i]];
+              bytes = 0;
+              bytesNeeded = 4;
+              state = 2;
+            }
+            else if (state == 2) {
+              bytes = (bytes << 8) | data[i];
+              bytesNeeded--;
+              if (bytesNeeded == 0) {
+                command = -1;
+                room.inventory[String(extra[0])] = bytes;
+                changed = true;
+                if (oot.mw_progressive_items_state) mem.u32[oot.mw_progressive_items_state+extra[0]*4] = bytes;
+              }
+            }
+          }
           else {
             console.log('Unknown command: '+command);
             connection.close()
@@ -280,6 +369,7 @@ function setupRoom() {
       });
       connection.on('close', function() {
         connection = null;
+        connectionReady = false;
         console.log('Disconnected');
       });
     });
@@ -297,6 +387,7 @@ setInterval(function () {
   } catch (e) {
     try { connection.close(); } catch(e){}
     connection = null;
+    connectionReady = false;
   }
 }, 30000);
 
@@ -306,26 +397,79 @@ function initVars() {
     var rando = mem.u32[0x801C8464];
     if (rando != 0) {
       var coop = mem.u32[rando];
-      oot = mem.bindvars({ready:true,currentMode:oot.currentMode}, [
-        [ coop + 0, 'protocol_version', u32 ],
-        [ coop + 4, 'player_id', u8 ],
-        // [ coop + 5, 'player_name_id', u8 ],
-        [ coop + 6, 'incoming_player', u16 ],
-        [ coop + 8, 'incoming_item', u16 ],
-        // [ coop + 10, 'mw_send_own_items', u8 ],
-        [ coop + 12, 'outgoing_key', u32 ],
-        [ coop + 16, 'outgoing_item', u16 ],
-        [ coop + 18, 'outgoing_player', u16 ],
-        [ save_context + 0x90, 'internal_count', u16 ]
+      oot = mem.bindvars({
+        ready:true,
+        currentMode:oot.currentMode,
+        player_names_addr:0,
+        mw_progressive_items_state:0,
+        version:'',
+        time:'',
+        worlds:0,
+        hash:[],
+        name:'',
+        inventory:0
+      }, [
+        [ coop + 0x0000, 'protocol_version', u32 ],
+        [ coop + 0x0004, 'player_id', u8 ],
+        // [ coop + 0x0005, 'player_name_id', u8 ],
+        [ coop + 0x0006, 'incoming_player', u16 ],
+        [ coop + 0x0008, 'incoming_item', u16 ],
+        [ coop + 0x000A, 'mw_send_own_items', u8 ],
+        [ coop + 0x000B, 'mw_progressive_items_enable', u8 ],
+        [ coop + 0x000C, 'outgoing_key', u32 ],
+        [ coop + 0x0010, 'outgoing_item', u16 ],
+        [ coop + 0x0012, 'outgoing_player', u16 ],
+        [ save_context + 0x90, 'internal_count', u16 ],
       ]);
-      player_names_addr = coop + 20;
-
-      if (oot.protocol_version != 4) {
-        console.log('This ROM is not compatible with this version of the co-op script.');
+      var cosmetic = mem.u32[rando+0x04];
+      if (oot.protocol_version != 6 || mem.u32[cosmetic] != 0x1F073FE0) {
+        console.clear();
+        console.log('This ROM is not compatible with this version of the Multiworld script.');
         oot.ready = false;
+        script.abort();
         return false;
       }
+      oot.player_names_addr = coop + 0x14;
+      oot.mw_progressive_items_state = coop + 0x81C;
+      oot.name = ootDecode(mem.u32[save_context + 0x28], true);
+      oot.name = ootDecode(mem.u32[save_context + 0x24], oot.name == '')+oot.name;
+      for (var i = 0; i < 0x24; i++) {
+        var c = mem.u8[cosmetic+0xAC+i];
+        if (c == 0x00) break;
+        oot.version += String.fromCharCode(c);
+      }
+      for (var i = 0; i < 0x24; i++) {
+        var c = mem.u8[cosmetic+0xE0+i];
+        if (c == 0x00) break;
+        oot.time += String.fromCharCode(c);
+      }
+      var worlds = '';
+      for (var i = 0; i < 0x10; i++) {
+        var c = mem.u8[cosmetic+0xD0+i];
+        if (c == 0x00) break;
+        if (c == 0x20) {
+          worlds = '';
+          continue;
+        }
+        worlds += String.fromCharCode(c);
+      }
+      oot.worlds = +worlds;
+      if (oot.worlds == 0) {
+        console.clear();
+        console.log('This is not a Multiworld ROM.');
+        oot.ready = false;
+        script.abort();
+        return false;
+      }
+      oot.hash.push(mem.u8[coop + 0x0814]);
+      oot.hash.push(mem.u8[coop + 0x0815]);
+      oot.hash.push(mem.u8[coop + 0x0816]);
+      oot.hash.push(mem.u8[coop + 0x0817]);
+      oot.hash.push(mem.u8[coop + 0x0818]);
+      oot.mw_send_own_items = 1;
+      oot.mw_progressive_items_enable = 1;
       loadState();
+      room.ids[oot.player_id] = oot.name;
       changed = true;
       if (!saveState()) {
         console.log('Unable to save Multiworld state!');
@@ -333,7 +477,7 @@ function initVars() {
         script.abort();
       }
       setupRoom();
-      writeNames();
+      writePlayerData();
       return true;
     }
   }
@@ -342,18 +486,19 @@ function initVars() {
 
 function writeName(i, name) {
   name = ootEncode(name, 8);
-  var addr = player_names_addr + (i)*8;
+  var addr = oot.player_names_addr + (i)*8;
   for (var n=0; n<name.length; n++) {
     mem.u8[addr] = name[n];
     addr++;
   }
 }
 
-function writeNames() {
-  for (var i = 1; i < 100; i++) {
+function writePlayerData() {
+  for (var i = 1; i < 0xFF; i++) {
     var id = String(i);
     if (room.ids[id]) writeName(i, room.ids[id]);
     else writeName(i, 'P'+i);
+    if (oot.mw_progressive_items_state && room.inventory[id]) mem.u32[oot.mw_progressive_items_state+id*4] = room.inventory[id];
   }
 }
 
@@ -375,7 +520,7 @@ function sendItem(item, playerID, location) {
       (item & 0xFF00) >> 8,
       (item & 0x00FF),
     ]
-    connection.write(new Buffer(buffer));
+    if (connectionReady) connection.write(new Buffer(buffer));
   }
   else if (item == 0x7C) {
     var buffer = [
@@ -389,7 +534,7 @@ function sendItem(item, playerID, location) {
       (item & 0xFF00) >> 8,
       (item & 0x00FF),
     ]
-    connection.write(new Buffer(buffer));
+    if (connectionReady) connection.write(new Buffer(buffer));
   }
 }
 
@@ -441,6 +586,7 @@ setInterval(function () {
         console.log('Will auto reconnect once in game again.');
       } catch(e){}
       connection = null;
+      connectionReady = false;
     }
     return;
   }
@@ -485,8 +631,41 @@ setInterval(function () {
         for (var actor = mem.u32[0x801CA11C]; actor; actor = mem.u32[actor+0x124]) {
           if (mem.u16[actor] == 0x017A && mem.s8[actor+0xAF] <= 0) {
             defeatedGanon = true;
-            if (connection) connection.write(new Buffer([0x05, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]));
+            if (connectionReady) connection.write(new Buffer([0x05, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]));
           }
+        }
+      }
+      if (oot.mw_progressive_items_state) {
+        var inventory = 0;
+        var val = mem.u8[0x8011A64D];
+        inventory |= (val == 0x0A ? 1 : (val == 0x0B ? 2 : 0)) <<  0; // hookshot
+        val = mem.u8[0x8011A673];
+        inventory |=                       ((val & 0xC0) >> 6) <<  2; // strength
+        inventory |=                       ((val & 0x18) >> 3) <<  4; // bomb bag
+        inventory |=                       ((val & 0x03) >> 0) <<  6; // bow
+        val = mem.u8[0x8011A672];
+        inventory |=                       ((val & 0xC0) >> 6) <<  8; // slingshot
+        inventory |=                       ((val & 0x30) >> 4) << 10; // wallet
+        inventory |=                       ((val & 0x06) >> 1) << 12; // scale
+        val = mem.u8[0x8011A671];
+        inventory |=                       ((val & 0x30) >> 4) << 14; // nuts
+        val = (val & 0x0E) >> 1;
+        if (val == 5) val = 2;
+        inventory |=                                       val << 16; // sticks
+        inventory |=                        mem.u8[0x8011A602] << 18; // magic
+        val = mem.u8[0x8011A64B];
+        inventory |= (val == 0x07 ? 1 : (val == 0x08 ? 2 : 0)) << 20; // ocarina
+        val = mem.u8[0x8011A64C];
+        inventory |=                   ((val == 0x09) ? 1 : 0) << 22; // bombchu bag
+        if (inventory != oot.inventory) {
+          oot.inventory = inventory;
+          if (connectionReady) connection.write(new Buffer([
+            0x07,
+            (inventory & 0xFF000000) >> 24,
+            (inventory & 0x00FF0000) >> 16,
+            (inventory & 0x0000FF00) >> 8,
+            (inventory & 0x000000FF),
+          ]));
         }
       }
     }
@@ -495,20 +674,24 @@ setInterval(function () {
 
 function setupRoomCfg() {
   console.clear();
-  console.log('Please enter your name (8 characters max).');
+  console.log('Would you like to use the default server? [y/n]');
   console.listen(function(input) {
-    if (consoleMode == 0) {
-      room.ids[oot.player_id] = input.substr(0, 8);
-      if (oot.ready) writeName(oot.player_id, room.ids[oot.player_id])
-      console.log('Hello '+room.ids[oot.player_id]+'.');
-      console.log('Would you like to use the default server? [y/n]');
-    }
-    else if (consoleMode == 1) {
+    // if (consoleMode == 0) {
+    //   room.ids[oot.player_id] = input.substr(0, 8);
+    //   if (oot.ready) writeName(oot.player_id, room.ids[oot.player_id])
+    //   console.log('Hello '+room.ids[oot.player_id]+'.');
+    //   console.log('Would you like to use the default server? [y/n]');
+    // }
+    if (consoleMode == 1) {
       if (inArray(input.toLowerCase(), ['y', 'yes'])) {
-        console.log('Please enter the room name.');
-        console.log('Use your randomizer seed if possible.');
-        console.log('Alphanumeric names only!');
-        consoleMode = 3;
+        // console.log('Please enter the room name.');
+        // console.log('Use your randomizer seed if possible.');
+        // console.log('Alphanumeric names only!');
+        // consoleMode = 3;
+
+        room.server = 'mw.auztin.net';
+        room.port = 9001;
+        consoleMode = 5;
       }
       else {
         console.log('Please enter the hostname or IP to connect to.');
@@ -520,9 +703,10 @@ function setupRoomCfg() {
     }
     else if (consoleMode == 3) {
       room.port = parseInt(input);
-      console.log('Please enter the room name.');
-      console.log('Use your randomizer seed if possible.');
-      console.log('Alphanumeric names only!');
+      // console.log('Please enter the room name.');
+      // console.log('Use your randomizer seed if possible.');
+      // console.log('Alphanumeric names only!');
+      consoleMode = 5;
     }
     else if (consoleMode == 4) {
       room.name = input;
